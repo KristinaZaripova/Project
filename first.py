7f25128e8b7c27a27c4115540051174502e70f5e
Kristina Timofeeva <timofeevakrist89@gmail.com>
2019-03-24 10:51:32 +0300

    created first programme

---
+++ first.py
@@ -0,0 +1,14 @@
+from datetime import datetime # мы импортируем встроенную функциональность из стандартной библиотеки
+
+odds = [1, 3, 5,7,9,11,13,15,17,19, 21, 23, 25, 27, 29]
+
+print(4/0)
+right_this_minute = datetime.today().minute
+print(right_this_minute)
+if right_this_minute in odds:
+    print("minute is odd")
+else:
+    print("minute is not odd")
+
+
+
